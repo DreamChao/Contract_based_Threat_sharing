23ba34fe4007ab3c57917143362df481f947a184
贺超 <plhechao@163.com>
2023-05-15 09:44:23 +0800

    first commit

---
+++ threat_sharing_contract.py
@@ -0,0 +1,19 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import optimize
+
+def border():
+    print()
+    print("=========================================")
+    print()
+
+
+    '''
+    需要画的四个图：
+        1.不同信息类型下，车辆效用于 interaction level 之间的关系图
+        2.不同信息类型下，IDS收益，也就是 防御效率于车辆数量之间的关系图
+        3. 
+
+    '''
+def
